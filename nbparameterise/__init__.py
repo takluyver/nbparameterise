966560d0414304f8a7a82f6fa6b2733ed435f6a6
Thomas Kluyver <thomas@kluyver.me.uk>
2020-07-03 10:00:22 +0100

    Version number -> 0.4

--- nbparameterise/__init__.py
+++ nbparameterise/__init__.py
@@ -1,6 +1,6 @@
 """Re-run a notebook substituting input parameters in the first cell."""
 
-__version__ = '0.3'
+__version__ = '0.4'
 
 from .code import (
     Parameter, extract_parameters, replace_definitions, parameter_values,
